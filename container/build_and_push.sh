d1c16478121dee5e98f149053169095f885fc9ad
dbinoy <dbinoy@gmail.com>
2018-04-18 13:21:40 -0700

    Fix reference to train and serve files in build and push script

--- container/build_and_push.sh
+++ container/build_and_push.sh
@@ -13,8 +13,8 @@ then
     exit 1
 fi
 
-chmod +x lstm/train
-chmod +x lstm/serve
+chmod +x byoa/train
+chmod +x byoa/serve
 
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
